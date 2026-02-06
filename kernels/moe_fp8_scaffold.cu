236d50c29d6b5038fd69e8aa4d988d234ad96f5f
rkpinto <rkpinto7@gmail.com>
2026-02-05 20:54:37 -0800

    Add MoE FP8 kernel scaffold

---
+++ kernels/moe_fp8_scaffold.cu
@@ -0,0 +1,9 @@
+// MoE FP8 kernel scaffold for MLSys 2026 NVIDIA Track (independent submission)
+// TODO: integrate with starter kit interface
+// TODO: expert routing / grouping
+// TODO: fused epilogue (activation, scaling)
+// TODO: benchmark + correctness
+
+extern "C" __global__ void moe_fp8_scaffold() {
+    // placeholder
+}
